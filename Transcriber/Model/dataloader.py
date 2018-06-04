Introduce Scheduled sampling. Fix Masking Bug. Another Hparameter search

Transcriber/Model/dataloader.py
@@ -55,10 +55,10 @@ class TranscriptionDataset(Dataset):
     ):
         self.cur_split_images = np.load(data_path).item()[split]
         if debug:
-            self.images = [os.path.join(image_dir, '1_' + f) for f in self.cur_split_images[-2:]]
-            self.images += [os.path.join(image_dir, '0_' + f) for f in self.cur_split_images[:2]]
+            self.images = [os.path.join(image_dir, '1_' + f) for f in self.cur_split_images[-5:]]
+            self.images += [os.path.join(image_dir, '0_' + f) for f in self.cur_split_images[:5]]
         else:
-            self.images = [os.path.join(image_dir, '1_' + f) for f in self.cur_split_images[-100:]]
+            self.images = [os.path.join(image_dir, '1_' + f) for f in self.cur_split_images[:500]]
             self.images += [os.path.join(image_dir, '0_' + f) for f in self.cur_split_images[-500:]]
 
         shuffle(self.images)
@@ -118,8 +118,9 @@ def collate_fn(data):
     images, captions, aux_labels = zip(*data)
     images = torch.stack(images, 0)
     lengths = [len(caption) for caption in captions]
-    targets = torch.zeros(len(captions), max(lengths)).long()
+    targets = torch.from_numpy(np.full((len(captions), max(lengths)), 622))
     for i, caption in enumerate(captions):
         end = lengths[i]
         targets[i, :end] = caption[:end]
+
     return images, targets, lengths, torch.Tensor(aux_labels)

Transcriber/Model/decoder_net.py
@@ -1,4 +1,5 @@
 import torch
+import random
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
@@ -21,23 +22,39 @@ class DecoderNet(nn.Module):
         self.linear = nn.Linear(hidden_size, output_size)
         self.max_length = max_length
         self.word_vectors = word_vectors
+        self.output_size = output_size
         self._init()
 
     def _init(self):
         self.embed.weight.data = self.word_vectors
 
     # Foward and sample stolen from https://github.com/yunjey/pytorch-tutorial
-    def forward(self, features, captions, lengths):
+    def forward(self, features, captions, lengths, teacher_forcing_ratio=0.5):
         """Decode image feature vectors and generate captions."""
+        use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+        loss = 0.0;
         embeddings = self.embed(captions)
-        # print('Embeddings before = ', features.shape, embeddings.shape)
         features = features.unsqueeze(1).expand(embeddings.shape)
         embeddings = torch.cat((features, embeddings), -1)
-        # print('Embeddings foward = ', embeddings.shape)
-        packed = pack_padded_sequence(embeddings, lengths, batch_first=True)
-        hiddens, _ = self.gru(packed)
-        outputs = self.linear(hiddens[0])
-        return outputs
+
+        states = None; weights = torch.ones(self.output_size); weights[622] = 0
+        for t in range(embeddings.shape[1]):
+            if t == 0: inputs = embeddings[:, t, :].unsqueeze(1)
+            hiddens, states = self.gru(inputs, states)
+            cur_output = self.linear(hiddens.squeeze(1))
+            _, predicted = cur_output.max(1)
+
+            loss_function = nn.CrossEntropyLoss(weight=weights, reduce=False)
+            if not use_teacher_forcing:
+                predicted = predicted.detach()
+                inputs = self.embed(predicted).unsqueeze(1)
+                inputs = torch.cat((features[:, t, :].unsqueeze(1), inputs), -1)
+                loss += loss_function(cur_output, captions[:, t])
+            else:
+                inputs = embeddings[:, t, :].unsqueeze(1)
+                loss += loss_function(cur_output, captions[:, t])
+        return loss
+
 
     def sample(self, features, states=None):
         """Generate captions for given image features using greedy search."""
@@ -48,11 +65,11 @@ class DecoderNet(nn.Module):
         embeddings = torch.cat((features, inputs), -1)
 
         for i in range(self.max_length):
-            hiddens, states = self.gru(embeddings, states)          # hiddens: (batch_size, 1, hidden_size)
+            hiddens, states = self.gru(embeddings, states)       # hiddens: (batch_size, 1, hidden_size)
             outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
             _, predicted = outputs.max(1)                        # predicted: (batch_size)
             sampled_ids.append(predicted)
-            embeddings = self.embed(predicted).unsqueeze(1)                       # inputs: (batch_size, embed_size)
-            embeddings = torch.cat((features, embeddings), -1)      # inputs: (batch_size, 1, embed_size)
+            embeddings = self.embed(predicted).unsqueeze(1)      # inputs: (batch_size, embed_size)
+            embeddings = torch.cat((features, embeddings), -1)   # inputs: (batch_size, 1, embed_size)
         sampled_ids = torch.stack(sampled_ids, 1)                # sampled_ids: (batch_size, max_length)
         return sampled_ids

Transcriber/train.py
@@ -22,7 +22,10 @@ else:
 dtype=torch.float32
 
 LARGE_NUMBER = 1e5
-LOG_OF_CAPTIONS = '../Data/FullData/LOG_OF_CAPTIONS.txt'
+LOG_OF_CAPTIONS = {
+    'train' : '../Data/FullData/LOG_OF_CAPTIONS.txt',
+    'dev' : '../Data/FullData/LOG_OF_DEV_CAPTIONS.txt'
+}
 def pre_train_encoder(
     encoder, optimizer, train_data_loader, dev_data_loader,
     restore_path='best_encoder_model.tar', save=True,
@@ -130,10 +133,11 @@ def train_transcriber(
     decoder = decoder.to(device)
     for i in range(epochs):
         for images, captions, lengths, aux_labels in tqdm(train_data_loader):
-            outputs, targets, train_encodings, true_captions, class_ = (
+            loss1, _, train_encodings, true_captions, class_ = (
                 forward(images, captions, lengths, encoder, decoder)
             )
-            loss = calculate_loss(outputs, targets, class_, aux_labels)
+            loss1 = (sum(loss1) / sum(lengths))
+            loss = calculate_loss(loss1, class_, aux_labels)
             encoder.zero_grad(); decoder.zero_grad()
             loss.backward()
             optimizer.step()
@@ -144,7 +148,6 @@ def train_transcriber(
                 evaluate_on_dev(dev_data_loader, encoder, decoder, train_dataset, dev_dataset)
             )
             train_blu = calculate_bleu_score(decoder, train_encodings, true_captions, train_dataset, dev_dataset)
-            del outputs, targets, train_encodings, true_captions
 
             train_losses.append(loss.item())
             dev_losses.append(avg_dev_loss)
@@ -173,27 +176,28 @@ def train_transcriber(
 def forward(images, captions, lengths, encoder, decoder):
     images = images.to(device=device, dtype=dtype)
     captions = captions.to(device=device)
-    targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
+    # print('Captions shape = ', captions.shape)
+    # targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
     encoding, _, class_ = encoder(images)
-    outputs = decoder(encoding, captions, lengths)
-    return outputs, targets, encoding, captions, class_
+    loss = decoder(encoding, captions, lengths)
+    return loss, None, encoding, captions, class_
 
 
 def evaluate_on_dev(loader, encoder, decoder, train_dataset, dev_dataset):
     encoder.eval(); decoder.eval()
     losses = []
     for images, captions, lengths, aux_labels in loader:
-        outputs, targets, dev_encodings, true_captions, class_ = (
+        loss1, _, dev_encodings, true_captions, class_ = (
             forward(images, captions, lengths, encoder, decoder)
         )
-        loss = calculate_loss(outputs, targets, class_, aux_labels)
+        loss1 = (sum(loss1) / sum(lengths))
+        loss = calculate_loss(loss1, class_, aux_labels)
         losses.append(loss.item())
-    dev_blu = calculate_bleu_score(decoder, dev_encodings, true_captions, train_dataset, dev_dataset)
-    del outputs, targets, dev_encodings, true_captions
+    dev_blu = calculate_bleu_score(decoder, dev_encodings, true_captions, train_dataset, dev_dataset, key='dev')
     encoder.train(); decoder.train()
     return np.mean(np.array(losses)), dev_blu
 
-def calculate_bleu_score(decoder, features_batch, true_captions, train_dataset, dev_dataset):
+def calculate_bleu_score(decoder, features_batch, true_captions, train_dataset, dev_dataset, key='train'):
     bleu_scores = []
     if isinstance(decoder, torch.nn.DataParallel):
         sampled_ids = decoder.module.sample(features_batch)
@@ -202,7 +206,7 @@ def calculate_bleu_score(decoder, features_batch, true_captions, train_dataset, 
     sampled_ids = sampled_ids.cpu().numpy()
     true_captions = true_captions.cpu().numpy()
     smoothing_func = SmoothingFunction().method1
-    with open(LOG_OF_CAPTIONS, 'w+') as log:
+    with open(LOG_OF_CAPTIONS[key], 'w+') as log:
         for predicted, truth in zip(sampled_ids, true_captions):
             true_caption = get_words(truth, train_dataset)
             generated_caption = get_words(predicted, dev_dataset)
@@ -224,8 +228,7 @@ def get_words(indexes, dataset):
         if word == '<end>': break
     return words
 
-def calculate_loss(y_hat, y_truth, aux_y_hat, aux_y, lambdah=0.5):
-    loss_function = nn.CrossEntropyLoss()
+def calculate_loss(seq_loss,  aux_y_hat, aux_y, lambdah=0.5):
     aux_loss_function = nn.BCEWithLogitsLoss()
     aux_y_hat = aux_y_hat.to(device); aux_y = aux_y.to(device)
-    return loss_function(y_hat, y_truth) + lambdah*aux_loss_function(aux_y_hat.squeeze(), aux_y)
+    return seq_loss + lambdah*aux_loss_function(aux_y_hat.squeeze(), aux_y)